821ac071dc49b661f7d317a8322858d5e94fc92e
AnnaO1704 <a.obukhova2002@gmail.com>
2024-07-21 17:39:00 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_dz = 12
+number_of_hours = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours / number_of_dz
+print("Курс:",course_name,",всего задач:",number_of_dz ,",затрачено часов:",number_of_hours,",среднее время выполнения",time_for_one_task,"часа.")
